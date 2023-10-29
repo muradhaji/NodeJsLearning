00a74de2749f8242a4340506ebdc94d1372ad772
muradhaji <muradhajiyev42@icloud.com>
2023-10-29 23:44:47 +0400

    Introduction to Express | First App | Express Generator App

---
+++ myExpressApp/app.js
@@ -0,0 +1,24 @@
+const express = require(`express`);
+
+const app = express();
+const port = 3000;
+
+app.get(`/`, (req, res) => {
+  res.send('Hello World. Got a GET request.');
+});
+
+app.post('/', (req, res) => {
+  res.send(`Got a POST request.`);
+});
+
+app.put('/', (req, res) => {
+  res.send(`Got a PUT request.`);
+});
+
+app.delete('/', (req, res) => {
+  res.send(`Got a DELETE request.`);
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
