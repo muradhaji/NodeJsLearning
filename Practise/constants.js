ecd5180f722b6f92c8036f996539ecd2a5c27c53
muradhaji <murad.hajiyev00@gmail.com>
2024-01-07 00:49:20 +0400

    Module, Export, Require

---
+++ Practise/constants.js
@@ -0,0 +1,9 @@
+const carBrands = {
+  ford: 'Ford',
+  bmw: 'BMW',
+  mercedes: 'Mercedes',
+};
+
+const message = 'Hi, Friend!';
+
+module.exports = { carBrands, message };
---
+++ Practise/modules.js
@@ -0,0 +1,9 @@
+const { carBrands, message } = require(`./constants`);
+
+console.log({ carBrands, message });
+
+const os = require('os');
+
+console.log(os.tmpdir());
+console.log(os.homedir());
+console.log(os.hostname());
